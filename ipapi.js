8625001061bec836f4944f52b3939c1e0ae47107
Karllic <64030092+Karllic@users.noreply.github.com>
2020-04-27 12:00:21 +0800

    Update ipapi.js

--- ipapi.js
+++ ipapi.js
@@ -138,7 +138,7 @@
    else if(para=="HGC Global Communications Limited"){
    return "环球电讯" 
    }
-   else if(para=="Pccw IMS Limited"){
+   else if(para=="PCCW IMS Limited"){
    return "盈科电讯" 
    }
    else if(para=="AWS EC2 (us-west-2)"){
